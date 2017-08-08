f38fef3e92998fd6e20c77fa7f369c4848af41c4
c <chenxiawei@gmail.com>
2017-08-08 17:02:52 +0800

    add war deploy support

--- spring/idb/web/src/main/java/com/idb/web/WebApplication.java
+++ spring/idb/web/src/main/java/com/idb/web/WebApplication.java
@@ -3,12 +3,19 @@ package com.idb.web;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.idb.dao")
 @ComponentScan("com.idb")
-public class WebApplication {
+public class WebApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
     }
