a1507d08665eee92c0092df6379962656f999a19
c <chenxiawei@gmail.com>
2017-08-09 20:34:44 +0800

    add slf4j support

--- spring/idb/web/src/main/java/com/idb/web/controller/IndexController.java
+++ spring/idb/web/src/main/java/com/idb/web/controller/IndexController.java
@@ -2,11 +2,16 @@ package com.idb.web.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class IndexController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @RequestMapping("/")
     public String index() {
+        logger.error("test");
         return "index";
     }
 }
